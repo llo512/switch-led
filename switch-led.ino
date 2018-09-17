5b7e95f99ab6c0252f55cd80adede1a398667a01
Leonard Lo <llo512@gmail.com>
2018-09-16 23:22:32 -0700

    Implement switch1 as a particle variable

--- switch-led.ino
+++ switch-led.ino
@@ -1,22 +1,18 @@
+// Leonard Lo - Copyright 2018
+
 #include "app.h"
 
-// int sw1 = D1;
-// int led1 = D7;
 int sw_state = LOW;
 
 void setup() {
-
+  Particle.variable("switch1", sw_state);
   pinMode(SW1, INPUT_PULLUP);
   pinMode(LED1, OUTPUT);
-
 }
 
 
 void loop() {
-
   sw_state = digitalRead(SW1);
   digitalWrite(LED1, sw_state);
-
   delay(100);
-
 }
