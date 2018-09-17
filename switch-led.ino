1f71a5ef2f356c8541bc43a9cbbb458b2153719c
Leonard Lo <llo512@gmail.com>
2018-09-17 14:51:37 -0700

    Add Particle.publish of switch1-state events

--- switch-led.ino
+++ switch-led.ino
@@ -3,16 +3,28 @@
 #include "app.h"
 
 int sw_state = LOW;
+int last_sw_state = LOW;
 
 void setup() {
   Particle.variable("switch1", sw_state);
+  
   pinMode(SW1, INPUT_PULLUP);
   pinMode(LED1, OUTPUT);
 }
 
 
 void loop() {
-  sw_state = digitalRead(SW1);
-  digitalWrite(LED1, sw_state);
-  delay(100);
+  sw_state = digitalRead(SW1);    // monitor switch1
+  digitalWrite(LED1, sw_state);   // reflect switch1 state to LED1
+
+  if(sw_state != last_sw_state) {   // if we detect state change
+    last_sw_state = sw_state;       // update stored state
+
+    String str_state;
+    if(sw_state) str_state = "ON";  // gen string value for state
+    else str_state = "OFF";
+
+    Particle.publish("switch1-state", str_state, PRIVATE);
+  }
+  delay(250);   // limite max state changes to 4/sec
 }
